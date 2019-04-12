c79db39f0ffb4b81ee8452509e5d2cdfb4ee9d64
Guillaume Damiens <guillaume.damiens@etu.utc.fr>
2019-04-12 10:20:40 +0200

    started controllers

---
+++ src/QuestionnaireController.java
@@ -0,0 +1,59 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Hashtable;
+import Model.Questionnaire;
+
+@WebServlet(
+        name="QuestionnaireServlet"
+)
+public class QuestionnaireController extends HttpServlet {
+    private static Hashtable<Integer, Questionnaire> questionnaireTable = new Hashtable<Integer, Questionnaire>();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        questionnaireTable.put(questionnaireTable.size(), new Questionnaire(
+                1,
+                request.getParameter("nom"), // nom
+                true, // actif
+                1 // id_sujet
+        ));
+
+        response.setContentType("text/html; charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Controller:</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Question créée " + questionnaireTable.get(questionnaireTable.size()-1).toString() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
---
+++ src/ReponseController.java
@@ -0,0 +1,59 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Hashtable;
+import Model.Reponse;
+
+@WebServlet(
+        name="ReponseServlet"
+)
+public class ReponseController extends HttpServlet {
+    private static Hashtable<Integer, Reponse> reponseTable = new Hashtable<Integer, Reponse>();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        reponseTable.put(reponseTable.size(), new Reponse(
+                1,
+                request.getParameter("nom"), // nom
+                true, // actif
+                1 // id_sujet
+        ));
+
+        response.setContentType("text/html; charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Controller:</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Question créée " + reponseTable.get(reponseTable.size()-1).toString() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
