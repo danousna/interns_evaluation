f08ef2effdd578821ce9925238a396e70a1f4eaa
Natan Danous <natan.danous@gmail.com>
2019-05-04 15:31:09 +0200

    Fix some errors + rework SQL

--- db.sql
+++ db.sql
@@ -1,61 +1,66 @@
-CREATE TABLE utilisateur (
-    id int PRIMARY KEY NOT NULL,
-    email varchar(50),
-    mot_de_passe varchar(50),
-    nom varchar(50),
-    societe varchar(50),
-    telephone varchar(20),
-    date_creation date,
-    actif bit,
-    est_admin bit
+CREATE TABLE user (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email varchar(100),
+    password varchar(255),
+    name varchar(60),
+    company varchar(100),
+    phone varchar(30),
+    created_at date,
+    is_active bit,
+    is_admin bit
 );
 
-CREATE TABLE sujet (
-    id int PRIMARY KEY NOT NULL,
-    nom varchar(50)
+CREATE TABLE subject (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(100)
 );
 
-CREATE TABLE questionnaire (
-    id int PRIMARY KEY NOT NULL,
-    nom varchar(50),
-    actif bit,
-    id_sujet int,
-    FOREIGN KEY (id_sujet) REFERENCES sujet(id)
+CREATE TABLE quiz (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(100),
+    is_active bit,
+    subject_id int,
+
+    FOREIGN KEY (subject_id) REFERENCES subject(id)
 );
 
 CREATE TABLE question (
-    id int PRIMARY KEY NOT NULL,
-    enonce varchar(255),
-    actif bit,
-    ordre int,
-    id_questionnaire int,
-    FOREIGN KEY (id_questionnaire) REFERENCES questionnaire(id)
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    body varchar(255),
+    is_active bit,
+    'order' int,
+    quiz_id int,
+
+    FOREIGN KEY (quiz_id) REFERENCES quiz(id)
 );
 
-CREATE TABLE reponse (
-    id int PRIMARY KEY NOT NULL,
-    valeur varchar(255),
-    actif bit,
-    correct bit,
-    ordre int,
-    id_question int,
-    FOREIGN KEY (id_question) REFERENCES question(id)
+CREATE TABLE answer (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    body varchar(255),
+    is_active bit,
+    is_correct bit,
+    'order' int,
+    question_id int,
+
+    FOREIGN KEY (question_id) REFERENCES question(id)
 );
 
-CREATE TABLE reponse_utilisateur (
-    id_utilisateur int,
-    id_reponse int,
-    PRIMARY KEY (id_utilisateur, id_reponse),
-    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id),
-    FOREIGN KEY (id_reponse) REFERENCES reponse(id)
+CREATE TABLE user_answer (
+    user_id int,
+    answer_id int,
+
+    PRIMARY KEY (user_id, answer_id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (answer_id) REFERENCES answer(id)
 );
 
-CREATE TABLE parcours (
-    id int,
+CREATE TABLE course (
+    id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     score int,
-    date_debut date,
-    date_fin date,
-    id_questionnaire int,
-    FOREIGN KEY (id_questionnaire) REFERENCES questionnaire(id)
+    started_at date,
+    finished_at date,
+    quiz_id int,
+
+    FOREIGN KEY (quiz_id) REFERENCES quiz(id)
 );
 
