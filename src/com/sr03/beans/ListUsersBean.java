6c398d97e7a1d24b20c65a6be4fd22da0bddbf1e
Guillaume Damiens <guillaume.damiens@etu.utc.fr>
2019-05-18 04:21:00 +0200

    Activate and desactvate user

--- src/com/sr03/beans/ListUsersBean.java
+++ src/com/sr03/beans/ListUsersBean.java
@@ -5,15 +5,17 @@ import com.sr03.dao.UserDAO;
 import com.sr03.beans.User;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServlet;
 import java.util.List;
 
 @ManagedBean
 @ViewScoped
 public class ListUsersBean extends HttpServlet {
-    public List<User> users;
+    private List<User> users;
 
     private UserDAO userDao;
 
@@ -29,5 +31,12 @@ public class ListUsersBean extends HttpServlet {
 
     public void setUsers(List<User> u) {
         users = u;
+    }
+
+    public void changeUserActivity(long id) {
+        userDao.changeUserActivity(id);
+        User changedUser = userDao.get(id);
+        FacesMessage message = new FacesMessage( "User N°" + changedUser.getId() + " " + changedUser.getName() + " " + (changedUser.getIs_active() ? "ACTIVÉ" : "DÉSACTIVÉ"));
+        FacesContext.getCurrentInstance().addMessage( null, message );
     }
 }
--- src/com/sr03/beans/User.java
+++ src/com/sr03/beans/User.java
@@ -35,6 +35,10 @@ public class User implements Serializable {
         this.created_at = created_at;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
